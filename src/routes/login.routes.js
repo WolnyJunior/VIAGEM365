19968655da1dca421980f5334a0b532baa2fd875
WolnyJunior <soares.junior.dev@gmail.com>
2024-05-11 15:10:46 -0300

    :sparkles: feat: Finalizado - CRUD - destinos

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const LoginController = require("../controllers/LoginController");
+const verificarEmailCPF = require("../middlewares/verificarEmailCPF");
 
 const loginRoutes = new Router()
 
 loginRoutes.post('/', LoginController.login)
-loginRoutes.post('/cadastro', LoginController.cadastrar)
+loginRoutes.post('/cadastro', verificarEmailCPF, LoginController.cadastrar)
 
 
 module.exports = loginRoutes
